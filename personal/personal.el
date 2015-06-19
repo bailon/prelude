bccfac2a925226a17e1da90e11ef38121b6db0fb
Bruno Merino-Bailón <merino.bailon@gmail.com>
2015-06-19 15:50:45 +0200

    Removing some functions and organizing the code (again).

--- personal/personal.el
+++ personal/personal.el
@@ -26,77 +26,43 @@
 (load-theme 'wombat)
 
 
+;; Org-Mode ----------------------------------------------------------
+(prelude-require-package 'org-bullets)
 
-;; Hack for setting a fixed wrap column in visual-line-mode.
-;; Source: ohai-emacs
-(defun set-visual-wrap-column (new-wrap-column &optional buffer)
-  "Force visual line wrap at NEW-WRAP-COLUMN in BUFFER (defaults
-    to current buffer) by setting the right-hand margin on every
-    window that displays BUFFER.  A value of NIL or 0 for
-    NEW-WRAP-COLUMN disables this behavior."
-  (interactive (list (read-number "New visual wrap column, 0 to disable: " (or visual-wrap-column fill-column 0))))
-  (if (and (numberp new-wrap-column)
-           (zerop new-wrap-column))
-      (setq new-wrap-column nil))
-  (with-current-buffer (or buffer (current-buffer))
-    (visual-line-mode t)
-    (set (make-local-variable 'visual-wrap-column) new-wrap-column)
-    (add-hook 'window-configuration-change-hook 'update-visual-wrap-column nil t)
-    (let ((windows (get-buffer-window-list)))
-      (while windows
-        (when (window-live-p (car windows))
-          (with-selected-window (car windows)
-            (update-visual-wrap-column)))
-        (setq windows (cdr windows))))))
-
-(defun update-visual-wrap-column ()
-  if (not visual-wrap-column)
-  (set-window-margins nil nil))
-
-;; ...and make it available for org-mode
 (add-hook
  'org-mode-hook
  (lambda ()
    (visual-line-mode 1)
    (whitespace-mode -1)
-   ;;   (set-visual-wrap-column 80)
+   (turn-on-auto-fill)
+   (set-fill-column 80)
+   (org-bullets-mode 1)
    ))
 
 
-;; disable flycheck on org-mode --------------------------------------
-;;(setq-default flycheck-disabled-checkers
-;;              (append flycheck-disabled-checkers 'org-mode))
-
-
-
-
-;; line numbers ------------------------------------------------------
+;; Line Numbers ------------------------------------------------------
 ;; (require 'linum)
 ;; (global-linum-mode 1)
 ;; (setq linum-format " %3d ")
 
 
-;; fullscreen --------------------------------------------------------
+
+;; Full Screen -------------------------------------------------------
 (custom-set-variables
  '(initial-frame-alist (quote ((fullscreen . maximized)))))
 
 
 
-;; org-bullets -------------------------------------------------------
-(prelude-require-package 'org-bullets)
-(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
-
-
-;; snippets ----------------------------------------------------------
+;; Snippets ----------------------------------------------------------
 (prelude-require-package 'yasnippet)
 (yas-global-mode 1)
 
 
-;; date translation --------------------------------------------------
+;; Date Translation --------------------------------------------------
 (setq system-time-locale (getenv "LANG"))
 
 
-;; org-babel ---------------------------------------------------------
+;; Org-Babel ---------------------------------------------------------
 (custom-set-variables
  '(org-babel-load-languages (quote ((emacs-lisp . t)
                                     (python . t)
@@ -106,7 +72,7 @@
  '(org-confirm-babel-evaluate nil))
 
 
-;; org-mode - code block color ---------------------------------------
+;; Org-Mode - Code block color ---------------------------------------
 (defface org-block-begin-line
   '((t (:underline "#A7A6AA" :foreground "#FFFFFF" :background "#1b1b1b")))
   "Face used for the line delimiting the begin of source blocks.")
@@ -120,5 +86,5 @@
   "Face used for the line delimiting the end of source blocks.")
 
 
-;; technical artifacts -----------------------------------------------
+;; Technical Artifacts -----------------------------------------------
 (provide 'personal)
